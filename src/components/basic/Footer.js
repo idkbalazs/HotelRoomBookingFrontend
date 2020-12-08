84d0309d525bc2e5aa84b70733ba1bd6b6064554
Regina Olah <y3wiw6@inf.elte.hu>
2020-12-08 21:21:19 +0100

    changed footer link

--- src/components/basic/Footer.js
+++ src/components/basic/Footer.js
@@ -10,7 +10,7 @@ function Footer() {
         <Box mt={8} className={classes.root}>
             <Typography variant="body2" color="textSecondary" align="center">
                 {'Copyright © '}
-                <Link color="inherit" href="https://hotelroombookingbackend.herokuapp.com/">
+                <Link color="inherit" href="https://hotelroombookingfrontend.herokuapp.com/">
                     HotelRoomBooking
                 </Link>{' '}
                 {new Date().getFullYear()}
